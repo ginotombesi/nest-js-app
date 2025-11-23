45d8c2481696c4293bdc52bdd3042da801c5f50d
ginotombesi <ginotombesi@gmail.com>
2025-11-23 20:07:35 -0300

    Middlewares

--- src/tournaments/tournaments.module.ts
+++ src/tournaments/tournaments.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TournamentsController } from './tournaments.controller';
 import { TournamentsService } from './tournaments.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   controllers: [TournamentsController],
   providers: [TournamentsService]
 })
-export class TournamentsModule {}
+export class TournamentsModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+      // middleware para que solo se ejecute en /tournaments GET y /tournaments/:id GET 
+        consumer.apply(LoggerMiddleware).forRoutes({path: 'tournaments', method: RequestMethod.GET} , {path: 'tournaments/:id', method: RequestMethod.GET})
+        .apply(AuthMiddleware).forRoutes('tournaments');
+    }
+}
