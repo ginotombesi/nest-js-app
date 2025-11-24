d641e45d84c1efa34f62eb64af7ae95b39331ebf
ginotombesi <ginotombesi@gmail.com>
2025-11-23 22:26:30 -0300

    Prueba de resource para generar una REST API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TournamentsModule } from './tournaments/tournaments.module';
 import { SaludoModule } from './saludo/saludo.module';
+import { PagosModule } from './pagos/pagos.module';
 
 @Module({
-  imports: [TournamentsModule, SaludoModule],
+  imports: [TournamentsModule, SaludoModule, PagosModule],
 })
 export class AppModule {}
